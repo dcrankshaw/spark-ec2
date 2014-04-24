8e1006c5d2d697d11049f42c06a45d42dbb811dd
Dan Crankshaw <dscrankshaw@gmail.com>
2014-04-23 22:12:27 -0700

    Updated spark-env.sh template for new spark config settings. Addresses issue introduced by spark commit fb98488fc8e68cc84f6e0750fd4e9e29029879d2

--- templates/root/spark/conf/spark-env.sh
+++ templates/root/spark/conf/spark-env.sh
@@ -14,17 +14,18 @@ export SCALA_HOME="/root/scala"
 
 # Set Spark's memory per machine; note that you can also comment this out
 # and have the master's SPARK_MEM variable get passed to the workers.
-export SPARK_MEM={{default_spark_mem}}
+export SPARK_EXECUTOR_MEMORY={{default_spark_mem}}
+export SPARK_DRIVER_MEMORY={{default_spark_mem}}
 
 # Set JVM options and Spark Java properties
-SPARK_JAVA_OPTS+=" -Dspark.local.dir={{spark_local_dirs}}"
-export SPARK_JAVA_OPTS
+# SPARK_JAVA_OPTS+=" -Dspark.local.dir={{spark_local_dirs}}"
+SPARK_LOCAL_DIRS="{{spark_local_dirs}}"
 
 export HADOOP_HOME="/root/ephemeral-hdfs"
 export SPARK_LIBRARY_PATH="/root/ephemeral-hdfs/lib/native/"
 export SPARK_MASTER_IP={{active_master}}
 export MASTER=`cat /root/spark-ec2/cluster-url`
-export SPARK_CLASSPATH=$SPARK_CLASSPATH":/root/ephemeral-hdfs/conf"
+# export SPARK_CLASSPATH=$SPARK_CLASSPATH":/root/ephemeral-hdfs/conf"
 
 # Bind Spark's web UIs to this machine's public EC2 hostname:
 export SPARK_PUBLIC_DNS=`wget -q -O - http://169.254.169.254/latest/meta-data/public-hostname`
